0819cc13d8fe5193545674347498042f0c14b9f3
Pedro Traba De La Gandara <ptraba-d@c1r10s3.42madrid.com>
2025-07-14 19:16:12 +0200

    some project cleaning

--- pushswap/ft_printf/ft_printf.c
+++ pushswap/ft_printf/ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptraba-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 12:58:41 by ptraba-d          #+#    #+#             */
+/*   Updated: 2025/04/11 12:58:44 by ptraba-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include <stdarg.h>
 
@@ -6,6 +18,8 @@ int	ft_printf(char const *str, ...)
 	int		count;
 	va_list	args;
 
+	if (!str)
+		return (-1);
 	va_start(args, str);
 	count = 0;
 	while (*str)
